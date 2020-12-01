79efb4c682e885c4e5c8bd8ea674e17896a61416
har-y <jakub.harenda@gmail.com>
2020-12-01 23:12:20 +0100

    add: player bullet script

---
+++ Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+    [SerializeField] private float _bulletSpeed;
+
+    private Vector2 _position;
+    private Vector2 _max;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _bulletSpeed = 8f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BulletMove();
+        MovementLimits();
+    }
+
+    private void BulletMove()
+    {
+        _position = transform.position;
+        _position = new Vector2(_position.x, _position.y + _bulletSpeed * Time.deltaTime);
+
+        transform.position = _position;
+    }
+
+    private void MovementLimits()
+    {
+        _max = Camera.main.ViewportToWorldPoint(new Vector2(1f, 1f));
+
+        if (transform.position.y > _max.y)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
