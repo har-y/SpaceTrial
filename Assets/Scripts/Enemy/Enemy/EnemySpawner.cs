c36838411a5991d42ad6d8f1fe8276da08580679
har-y <jakub.harenda@gmail.com>
2020-12-04 00:56:17 +0100

    add: spawn multiple enemies (enemy spawner script)

--- Assets/Scripts/Enemy/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/Enemy/EnemySpawner.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [SerializeField] List<WaveConfig> _waveConfigs;
+
+    private WaveConfig _currentWave;
+
+    private int _startingWave = 0;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _currentWave = _waveConfigs[_startingWave];
+        StartCoroutine(SpawnEnemies(_currentWave));
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private IEnumerator SpawnEnemies(WaveConfig waveConfig)
+    {
+        for (int i = 0; i < waveConfig.GetEnemyNumber(); i++)
+        {
+            Instantiate(waveConfig.GetEnemyPrefab(), waveConfig.GetWaypoints()[0].transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(waveConfig.GetSpawnInterval());
+        }
     }
 }
