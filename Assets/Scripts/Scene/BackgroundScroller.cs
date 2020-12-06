4ed72fc969ba4c9594749a700b9d03cd6924277e
har-y <jakub.harenda@gmail.com>
2020-12-06 23:55:09 +0100

    add: scrolling background effect (background scroller script)

--- Assets/Scripts/Scene/BackgroundScroller.cs
+++ Assets/Scripts/Scene/BackgroundScroller.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class BackgroundScroller : MonoBehaviour
 {
+    [Header("Background Configuration")]
+    [SerializeField] float _backgroundSpeed = 0.25f;
+
+    private Material _material;
+    private Vector2 _offset;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _material = GetComponent<Renderer>().material;
+        _offset = new Vector2(0f, _backgroundSpeed);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _material.mainTextureOffset += _offset * Time.deltaTime;
     }
 }
