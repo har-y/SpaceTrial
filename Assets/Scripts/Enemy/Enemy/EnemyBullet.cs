fb886092f34575e5742a95c75bcca423dda629d5
har-y <jakub.harenda@gmail.com>
2020-12-06 22:31:02 +0100

    add: enemy shoot (enemy bullet script)

--- Assets/Scripts/Enemy/Enemy/EnemyBullet.cs
+++ Assets/Scripts/Enemy/Enemy/EnemyBullet.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class EnemyBullet : MonoBehaviour
 {
+    [SerializeField] private float _bulletSpeed;
+
+    private Vector2 _position;
+    private Vector2 _min;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _bulletSpeed = 5f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        BulletMovement();
+        BulletMovementLimits();
+    }
+
+    private void BulletMovement()
+    {
+        _position = transform.position;
+        _position.y -= _bulletSpeed * Time.deltaTime;
+
+        transform.position = _position;
+    }
+
+    private void BulletMovementLimits()
+    {
+        _min = Camera.main.ViewportToWorldPoint(new Vector2(0f, 0f));
+
+        if (transform.position.y < _min.y)
+        {
+            Destroy(gameObject);
+        }
     }
 }
