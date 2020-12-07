46ed1933fb87bbf61cc143ae08a92025d1a18a30
har-y <jakub.harenda@gmail.com>
2020-12-08 00:21:49 +0100

    add: load menu/level/game over scene (game controller script)

--- Assets/Scripts/Controller/GameController.cs
+++ Assets/Scripts/Controller/GameController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
@@ -14,5 +15,25 @@ public class GameController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadOver()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
